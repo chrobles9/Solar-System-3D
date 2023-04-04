a24636fe1f449bd7bb529d4cb13a1633241126f8
Carlos Robles <chrobles@pm.me>
2023-04-04 18:21:32 -0400

    Vite config file update.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 
 import { defineConfig } from "vite"
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
 
 // vite.config.js
 export default defineConfig ({
   // config options
   base: '/Solar-System-3D/',
-  plugins: [react()]
+  // plugins: [react()]
 })
