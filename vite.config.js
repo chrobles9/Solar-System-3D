5fe4b928d5df04eb2e10b514ff3932ba11345765
Carlos Robles <chrobles@pm.me>
2023-04-04 18:05:14 -0400

    Vite config file created.

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+
+import { defineConfig } from "vite"
+import react from '@vitejs/plugin-react'
+
+// vite.config.js
+export default defineConfig ({
+  // config options
+  base: '/Solar-System-3d/',
+  plugins: [react()]
+})
